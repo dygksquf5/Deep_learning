85902e8469d98fb51e2431932eba34d564826bf2
dygksquf5 <66229916+dygksquf5@users.noreply.github.com>
2020-12-15 14:33:41 +0900

    study

--- functions/func.py
+++ functions/func.py
@@ -13,6 +13,10 @@ def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
 
+def sigmoid_grad(x):
+    return (1.0 - sigmoid(x)) * sigmoid(x)
+
+
 # def softmax(a):
 #     c = np.max(a)
 #     exp_a = np.exp(a - c) # 오버플로 방지
